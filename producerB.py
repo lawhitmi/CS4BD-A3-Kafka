1043ca20d51c79ff7da5732a799727b885a16e1b
lawhitmi <lucas.whitmire@gmail.com>
2019-12-17 21:05:35 +0100

    Added loop to send odd numbers every other second

--- producerB.py
+++ producerB.py
@@ -1,3 +1,11 @@
 from kafka import KafkaProducer
+import time
 
-producer = KafkaProducer()
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+starttime = time.time()
+i = -1
+while True:
+    i += 2
+    producer.send('test',i)
+    time.sleep(2.0 - ((time.time()-starttime)%2))
