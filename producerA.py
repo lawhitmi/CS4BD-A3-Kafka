ca47ad4fce47d3031778f0276545f58392347770
lawhitmi <lucas.whitmire@gmail.com>
2019-12-17 20:47:03 +0100

    Added loop to send even numbers every second

--- producerA.py
+++ producerA.py
@@ -1,3 +1,11 @@
 from kafka import KafkaProducer
+import time
 
-producer = KafkaProducer(bootstrap_servers='10.0.2.15:7203')
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+starttime = time.time()
+i = 0
+while True:
+    i += 2
+    producer.send('test',i)
+    time.sleep(1.0 - ((time.time()-starttime)%1))
